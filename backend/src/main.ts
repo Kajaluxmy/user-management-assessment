353b16ed887923d5ca7a4c681ac67c98916bb6ef
Kajaluxmy <kajalluxsi2003@gmail.com>
2026-09-22 22:30:40 +0530

    feat: add user schema

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,14 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
-}
-void bootstrap();
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    MongooseModule.forRoot(process.env.MONGO_URI as string),
+  ],
+})
+export class AppModule {}
